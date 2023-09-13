6e6e9b7094f0a255a13e4422122f09a65bcdf079
Julia-slvbrg <julia.slvb@gmail.com>
2023-09-13 19:29:23 -0300

    feat: implemented tests for GetUsers and Signup

--- src/services/TokenManager.ts
+++ src/services/TokenManager.ts
@@ -1,17 +1,11 @@
-import dotenv from "dotenv";
 import jwt from "jsonwebtoken";
-import { USER_ROLES } from "../models/User";
+import dotenv from "dotenv";
+import { TokenPayload } from "../models/User";
 
 dotenv.config();
 
-export interface TokenPayload {
-    id: string,
-    role: USER_ROLES,
-    name: string
-};
-
 export class TokenManager {
-    public createToken(payload: TokenPayload):string{
+    public createToken = (payload:TokenPayload): string => {
         const token = jwt.sign(
             payload,
             process.env.JWT_KEY as string,
@@ -19,17 +13,19 @@ export class TokenManager {
                 expiresIn: process.env.JWT_EXPIRES_IN
             }
         )
+
         return token
     };
 
-    public getPayload(token: string): TokenPayload | null {
+    public getPayload = (token:string): TokenPayload | null => {
         try {
             const payload = jwt.verify(token, process.env.JWT_KEY as string);
-
             return payload as TokenPayload
-            
+
         } catch (error) {
             return null
         }
     }
 }
+
+
---
+++ tests/business/users/GetUsers.test.ts
@@ -0,0 +1,127 @@
+import { ZodError } from "zod";
+import { UserBusiness } from "../../../src/business/users/UserBusiness";
+import { GetUsersSchema } from "../../../src/dtos/users/getUsers.dto";
+import { BadRequestError } from "../../../src/errors/BadRequestError";
+import { BaseError } from "../../../src/errors/BaseError";
+import { USER_ROLES } from "../../../src/models/User";
+import { HashManagerMock } from "../../mocks/HashManagerMock";
+import { IdGeneratorMock } from "../../mocks/IdGeneratorMock";
+import { TokenManagerMock } from "../../mocks/TokenManagerMock";
+import { UserDatabaseMock } from "../../mocks/UserDatabaseMock";
+
+describe('Test for the GetUsers method', () => {
+    const userBusiness = new UserBusiness(
+        new UserDatabaseMock(),
+        new TokenManagerMock(),
+        new IdGeneratorMock(),
+        new HashManagerMock()
+    );
+
+    test('Should return a list containing all users', async () => {
+        const input = GetUsersSchema.parse({
+            token: "token-mock-adminUser"
+        });
+
+        const output = await userBusiness.getUsers(input);
+        
+        expect(output).toHaveLength(2);
+        expect(output).toEqual([ 
+            {
+                id: 'id-mock-normUser',
+                name: 'NormUser',
+                email: 'normuser@email.com',
+                role: USER_ROLES.NORMAL,
+                createdAt: expect.any(String)
+            },
+            {
+                id: 'id-mock-adminUser',
+                name: 'AdminUser',
+                email: 'adminuser@email.com', 
+                role: USER_ROLES.ADMIN,
+                createdAt: expect.any(String)
+            }
+        ])
+    });
+    
+    test('Should return one user for the query', async () => {
+        const input = GetUsersSchema.parse({
+            q: 'norm',
+            token: "token-mock-adminUser"
+        });
+
+        const output = await userBusiness.getUsers(input);
+
+        expect(output).toEqual([
+            {
+                id: 'id-mock-normUser',
+                name: 'NormUser',
+                email: 'normuser@email.com',
+                role: USER_ROLES.NORMAL,
+                createdAt: expect.any(String)
+            }
+        ])
+    });
+
+    test('Unauthorized user', async () => {
+        expect.assertions(2);
+        try {
+            const input = GetUsersSchema.parse({
+                token: 'token-mock-normUser'
+            });
+
+            await userBusiness.getUsers(input)
+
+        } catch (error) {
+            if(error instanceof BadRequestError){
+                expect(error.statusCode).toBe(400);
+                expect(error.message).toBe('Only ADMIN users can access users information.')
+            }
+        }
+    });
+
+    test('Unauthorized token (token without payload)', async () => {
+        expect.assertions(2);
+        try {
+            const input = GetUsersSchema.parse({
+                token: 'token-mock'
+            });
+
+            await userBusiness.getUsers(input)
+
+        } catch (error) {
+            if(error instanceof BadRequestError){
+                expect(error.statusCode).toBe(400);
+                expect(error.message).toBe('token is required.')
+            }
+        }
+    });
+
+    test('Zod validation for the token', async () => {
+        expect.assertions(1);
+        try {
+            const input = GetUsersSchema.parse({
+                token: 111
+            });
+
+            await userBusiness.getUsers(input)
+
+        } catch (error) {
+            expect(error instanceof ZodError).toBe(true)
+        }
+    });
+
+    test('Zod validation for the query', async () => {
+        expect.assertions(1);
+        try {
+            const input = GetUsersSchema.parse({
+                q: 111,
+                token: "token-mock-adminUser"
+            });
+
+            await userBusiness.getUsers(input)
+
+        } catch (error) {
+            expect(error instanceof ZodError).toBe(true)
+        }
+    })
+})
